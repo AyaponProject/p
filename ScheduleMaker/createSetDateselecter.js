2a0d27f4c36cfacba8e48abaf4416e2561b4651e
染谷 <103508288+somememeeee@users.noreply.github.com>
2025-05-20 23:04:19 +0900

    セレクタ

---
+++ ScheduleMaker/createSetDateselecter.js
@@ -0,0 +1,12 @@
+const n = 7; // 必要なセレクト数
+const container = document.getElementById('container');
+const template = document.getElementById('select-template');
+
+for (let i = 1; i <= n; i++) {
+    // テンプレートを複製
+    const clone = template.cloneNode(true);
+    clone.id = `select-${i}`;
+    clone.style.display = 'inline'; // 表示する
+    container.appendChild(clone);
+    container.appendChild(document.createElement('br')); // 改行用
+}
